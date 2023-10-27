0c0e99562d58f80c723a9e956c2f190512a4734b
Aaron Savage <blain.aaron@gmail.com>
2023-10-26 22:16:56 -0400

    Refine drawing code

--- term.c
+++ term.c
@@ -7,6 +7,7 @@
 #include <sys/ioctl.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/mman.h>
 
 #include "fractal_noise.h"
 #include "constants.h"
@@ -179,7 +180,8 @@ void paint_cell(Canvas* canvas, int x, int y, int index){
 
 
 int main(){
-	FILE* tty = fopen(ttyname(STDIN_FILENO), "w");
+	//FILE* tty = fopen(ttyname(STDIN_FILENO), "w");
+	int tty = open(ttyname(STDIN_FILENO), O_RDWR | O_SYNC);
 
 	srand(time(NULL));
 	init_palette();
@@ -236,6 +238,8 @@ int main(){
 			3 + // Signal to reset cursor
 			1; // Room for null terminator
     char* buffer = malloc(buf_size);
+		//char* buffer = mmap(NULL, buf_size, 
+	//			PROT_WRITE | PROT_READ, 0, tty, 0);
 
     int quit = 0;
     while(!quit){
@@ -244,7 +248,6 @@ int main(){
 				for(int y=0; y<term_height; y++){
 					for(int x=0; x<term_width; x++){
 						int offset = x + y * MAX_VIEW_WIDTH;
-						/*
 							canvas->cells[offset].character = 
 							texture[(noise[offset] + ticks) / 8 % 4];
 							int color = 
@@ -257,21 +260,20 @@ int main(){
 							color += brightness;
 							canvas->cells[offset].color = color;
 							canvas->cells[offset].bg_color = ticks / 128 % 8 + 100;
-							*/
 						paint_cell(canvas, x, y,
-								(noise[offset] + ticks) / 8 % 200);
+								(noise[offset] + ticks) / 12 % 200);
 					}
 				}
 				/*
-				for(int j=1; j<16; j++){
-				for(int i=0; i<term_width - 1; i++){
+				for(int j=0; j<term_height/2; j++){
+				for(int i=0; i<92; i++){
 					paint_cell(canvas, i, j, i);
 				}
 				}
 
-				for(int j=16; j<32; j++){
-				for(int i=term_width; i<200; i++){
-					paint_cell(canvas, i - term_width, j, i);
+				for(int j=term_height/2; j<term_height; j++){
+				for(int i=92; i<200; i++){
+					paint_cell(canvas, i - 92, j, i);
 				}
 				}
 				*/
@@ -280,7 +282,7 @@ int main(){
         ticks++;
 		usleep(1000000/60);
     }
-		fflush(tty);
+		//fflush(tty);
     end_term();
 		printf("Term dimensions: %d x %d\n", term_width, term_height);
 		printf("Biggest buffer: %d\n", biggest_buffer);
@@ -290,7 +292,7 @@ int main(){
 
 
 void term_refresh(char* buffer, Canvas* canvas, Canvas* old_canvas,
-		FILE* tty){
+		int tty){
 	// None of this uses C std string functions. Instead I build the
 	// output buffer by inserting the new character and incrementing
 	// the insertion pointer.
@@ -310,18 +312,20 @@ void term_refresh(char* buffer, Canvas* canvas, Canvas* old_canvas,
 		ADD('['); \
 		ADD(dist + '0'); \
 		ADD('C');
-	#define MOVE_10(dist) \
+	#define MOVE_10(dist){ \
 		ADD('\x1b'); \
 		ADD('['); \
 		ADD(dist / 10 + '0'); \
 		ADD(dist % 10 + '0'); \
-		ADD('C')
-	#define MOVE_100(dist) \
+		ADD('C'); \
+	}
+	#define MOVE_100(dist){ \
 		ADD('\x1b'); \
 		ADD('['); \
 		ADD(dist / 100 + '0'); \
 		ADD((dist % 100) / 10 + '0'); \
-		ADD((dist % 100) % 10 + '0');
+		ADD((dist % 100) % 10 + '0'); \
+	}
 	buffer[0] = '\x1b';
 	buffer[1] = '[';
 	buffer[2] = 'H';
@@ -334,7 +338,12 @@ void term_refresh(char* buffer, Canvas* canvas, Canvas* old_canvas,
 	for(int y = 0; y < term_height; y++){
 		fg_color = -1;
 		bg_color = -1;
-		for(int x = 0; x <= term_width; x++){
+		char temp[12];
+		sprintf(temp, "\x1b[m");
+		for(int i=0; i<strlen(temp); i++){
+			ADD(temp[i]);
+		}
+		for(int x = 0; x < term_width; x++){
 			int offset = x + y * MAX_VIEW_WIDTH;
 			int next_fg_color = canvas->cells[offset].color;
 			int next_bg_color = canvas->cells[offset].bg_color;
@@ -377,44 +386,45 @@ void term_refresh(char* buffer, Canvas* canvas, Canvas* old_canvas,
 			//int offset = x + y + MAX_VIEW_WIDTH;
 			char next_char = canvas->cells[x + y * MAX_VIEW_WIDTH].character;
 			char old_char = old_canvas->cells[x + y * MAX_VIEW_WIDTH].character;
+			
 			if(next_char == old_char &&
 					next_fg_color == old_fg &&
-					next_bg_color == old_bg) skip_length++;
-			else{
-				if(skip_length > 0){
-					if(skip_length < 4 ){
-						char skip_char;
-						int offset = x + y * MAX_VIEW_WIDTH;
-						if(skip_length == 3){
-							skip_char = canvas->cells[offset - 3].character;
-							ADD(skip_char);
-						}
-						if(skip_length >= 2){
-							skip_char = canvas->cells[offset - 2].character;
-							ADD(skip_char);
-						}
-						skip_char = canvas->cells[offset - 1].character;
-						ADD(skip_char);
-					} else if(skip_length >= 10 && skip_length >= 4){
-						MOVE_10(skip_length);
-					} else if(skip_length >= 100){
-						MOVE_100(skip_length);
-					} else {
-						MOVE(skip_length);
-					}
-				}
-				skip_length = 0;					
-				ADD(next_char);
+					next_bg_color == old_bg){
+					skip_length++;
+					//ADD(' ');
+					//ADD('\x1b');
+					//ADD('[');
+					//ADD('C');
+					continue;
+
 			}
-		}
-		if(skip_length > 0) {
-			ADD('\n');
+			char temp[12];
+			if(skip_length > 0){
+				//Why doesn't this work?
+				/*
+				if(skip_length < 6){
+					for(int i=0; i<skip_length; i++){
+						ADD(canvas->cells[offset - 1].character);
+					}
+				} else{
+				*/
+					ADD('\x1b');
+					ADD('[');
+					if(x > 99) ADD((x) / 100 + '0');
+					if(x > 9) ADD((x) % 100 / 10 + '0');
+					ADD((x) % 10 + '0');
+					ADD('G');
+				//}
+			}
+			ADD(next_char);
 			skip_length = 0;
 		}
+		ADD('\n');
 	}
 	// Cut off that last newline so the screen doesn't scroll
 	pointer--;
-	fwrite(buffer, 1, pointer - buffer, tty);
+	//fwrite(buffer, 1, pointer - buffer, tty);
+	write(tty, buffer, pointer - buffer);
 	if(smallest_buffer > pointer - buffer) smallest_buffer = pointer - buffer;
 	if(biggest_buffer < pointer - buffer) biggest_buffer = pointer - buffer;
 	
